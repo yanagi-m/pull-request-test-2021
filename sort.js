9009d6dedb30a7a2d6040d5cf5c9e8df5cae150c
yanagi-m <mari.yanagihara@esm.co.jp>
2021-04-06 14:23:46 +0900

    配列をソートする処理を追加しました

--- sort.js
+++ sort.js
@@ -1,5 +1,5 @@
 var sortNumber = function (number) {
-    number.sort(function (a. b){
+    number.sort(function (a, b){
         if(a === b){
             return 0;
         }
@@ -8,4 +8,5 @@ var sortNumber = function (number) {
 };
 
 var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
 console.log(number);
